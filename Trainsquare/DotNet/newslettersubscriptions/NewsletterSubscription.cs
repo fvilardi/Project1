249a4c0c59fbdadbc655bd9f677216ec877cf036
Frank Vilardi <97715225+fvilardi@users.noreply.github.com>
2022-05-21 14:25:20 -0700

    Add files via upload

---
+++ Trainsquare/DotNet/newslettersubscriptions/NewsletterSubscription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Domain
+{
+    public class NewsletterSubscription
+    {
+        public string Email { get; set; }
+        public bool IsSubscribed  { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
