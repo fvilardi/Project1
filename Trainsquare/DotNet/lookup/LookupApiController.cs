b320ec12ff9b85ca692c798648ea56f5e5b90634
Frank Vilardi <97715225+fvilardi@users.noreply.github.com>
2022-05-21 14:21:26 -0700

    Add files via upload

---
+++ Trainsquare/DotNet/lookup/LookupApiController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Web.Api.Controllers.LocationTypes
+{
+    [Route("api/lookup")]
+    [ApiController]
+    public class LookupApiController : BaseApiController
+    {
+        private ILookupsService _service = null;
+        private IAuthenticationService<int> _authService = null;
+        public LookupApiController(ILookupsService service
+            , ILogger<LookupApiController> logger
+            , IAuthenticationService<int> authService) : base(logger)
+        {
+            _service = service;
+            _authService = authService;
+        }
+
+        [HttpPost]
+        public ActionResult<ItemResponse<ExpandoObject>> GetTypes(IEnumerable<string> tableNames)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                ExpandoObject result = _service.GetTypes(tableNames);
+
+                if (result == null)
+                {
+                    code = 404;
+                    response = new ErrorResponse("App Resource not found");
+                }
+                else
+                {
+                    response = new ItemResponse<ExpandoObject> { Item = result };
+                }
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+                base.Logger.LogError(ex.ToString());
+            }
+
+            return StatusCode(code, response);
+
+        }
+
+    }
+}
